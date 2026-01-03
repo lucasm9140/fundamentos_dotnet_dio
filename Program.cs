eac829de55630723e3f9e6c699578714aa269139
lucasm9140 <lucas14fdk@gmail.com>
2026-01-03 16:20:41 -0300

    aula sobre operadores condicionais

--- Program.cs
+++ Program.cs
@@ -1,11 +1,134 @@
 ﻿// apontando o caminho do namespace da classe usando o using
+using System.Linq.Expressions;
 using fundamentos_dotnet_dio.Models;
+
+// Switch Case
+// Console.WriteLine("Digite Uma Letra: ");
+// string letra = Console.ReadLine();
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//     Console.WriteLine("Esta letra e uma vogal.");
+//     break;
+
+//     default:
+//     Console.WriteLine("Esta letra nao e uma vogal.");
+//     break;
+// }
+
+
+// Console.WriteLine("Digite Uma Letra: ");
+// string letra = Console.ReadLine();
+
+// if (letra == "a"||
+//     letra == "e"||
+//     letra == "i"||
+//     letra == "o"||
+//     letra == "u")
+// {
+//     Console.WriteLine("Esta letra e uma vogal.");
+// }
+// else
+// {
+//     Console.WriteLine("Esta letra nao e uma vogal.");
+// }
+
+
+// Console.WriteLine("Digite Uma Letra: ");
+// string letra = Console.ReadLine();
+
+// if (letra == "a")
+// {
+//     Console.WriteLine("Essa letra e uma vogal.");
+// }
+// else if (letra == "e")
+// {
+//     Console.WriteLine("Essa Letra e uma vogal");
+// }
+// else if (letra == "i")
+// {
+//     Console.WriteLine("Essa Letra e uma vogal");
+// }
+// else if (letra == "o")
+// {
+//     Console.WriteLine("Essa Letra e uma vogal");
+// }
+// else if (letra == "u")
+// {
+//     Console.WriteLine("Essa Letra e uma vogal");
+// }
+
+
+
+
+
+
+
+// Operador condicional
+// int quantidadeEmEstoque = 10;
+// int quantidadeEmCompra = 4;
+
+// if(quantidadeEmEstoque >= quantidadeEmCompra)
+// {
+//     Console.WriteLine("Venda Realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe, nao temos a quantidade desejada em estoque!");
+// }
+
+// Operador condicional, passando uma variavel booleana
+// int quantidadeEmEstoque = 10;
+// int quantidadeEmCompra = 4;
+// bool possivelVenda = quantidadeEmEstoque >= quantidadeEmCompra;
+
+// Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade em Compra: {quantidadeEmCompra}");
+// Console.WriteLine($"Será Possível Realizar a Venda: {possivelVenda}");
+
+// if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe, nao temos a quantidade em estoque!");
+// }
+//*************************************************************************************//
+//if aninhado
+// int quantidadeEmEstoque = 10;
+// int quantidadeEmCompra = 4;
+// bool possivelVenda = quantidadeEmCompra > 0 && quantidadeEmEstoque >= quantidadeEmCompra;
+
+// Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade em Compra: {quantidadeEmCompra}");
+// Console.WriteLine($"Será Possível Realizar a Venda: {possivelVenda}");
+
+// if (quantidadeEmCompra == 0)
+// {
+//     Console.WriteLine("Venda Invalida!");
+// }
+
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe, nao temos a quantidade em estoque!");
+// }
 
 
 // Conversao para tipo string
 //int inteiro = 5;
 //string a = inteiro.ToString(); // melhor pratica para converter para string.
 // nao tem parse para string, e o convert e redundante.
+//********************************************************************************//
 
 // Cast implicito
 // int a = 5;
@@ -16,7 +139,7 @@ using fundamentos_dotnet_dio.Models;
 // nao permite o casting implicito, sera necessario realizar a conversao.
 // long a = 5;
 // int b = Convert.ToInt32(a);
-
+//**********************************************************************************//
 
 // Cast - Casting
 // Converter uma string para inteiro.
@@ -24,11 +147,12 @@ using fundamentos_dotnet_dio.Models;
 //int a = Convert.ToInt32("5");
 // 2 forma
 // int a = int.Parse("5");
+//**********************************************************************************//
 
-// Diferencia entre convert e parse
+// Diferença entre convert e parse
 // parse nao aceita valor null.
 // convert ele aceita null e retorna 0.
-
+//***********************************************************************************//
 
 // Operador de atribuição(= sinal de atribuição, quando quer modificar o valor da variável.)
 // int a = 10;
