0af64518bd8422b7cb2b70621e3deb021eae692e
lucasm9140 <lucas14fdk@gmail.com>
2026-01-02 22:19:30 -0300

    aulas sobre variáveis, manipulação, uso datetime

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
 ﻿// apontando o caminho do namespace da classe usando o using
 using fundamentos_dotnet_dio.Models;
+
+// // pega a data atual da maquina em uso.
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual);
+
+
+
+// string aprensentacao = "Seja Bem Vindo";
+
+// int quantidade = 1;
+// //Manipulando a variavel
+// //quantidade = 10;
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(aprensentacao);
+// Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " + altura);
+// Console.WriteLine("Valor da variavel preco: " + preco);
+// Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+
+
 // instanciando a classe Pessoa
-Pessoa p1 = new Pessoa();
+// Pessoa p1 = new Pessoa();
 
-p1.Nome = "Dev. Lucas";
-p1.Idade = 28;
-p1.Apresentar();
+// p1.Nome = "Dev. Lucas";
+// p1.Idade = 28;
+// p1.Apresentar();
 
+// Declarando variaveis
 
+//string nome= "LUCAS" 
+//int idade = 1;
+//double altura = 1.90;
+//float altura = 1.90;
+// decimal = 1.80M; obs.: o M é obrigatório par contar a casa e o valor monetário.
+// bool condicao = true;
