dcde8807a15e6b045dc121515c800f2000090548
lucasm9140 <lucas14fdk@gmail.com>
2026-01-03 12:38:37 -0300

    Aula de Operadores de atribuicao

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
 ﻿// apontando o caminho do namespace da classe usando o using
 using fundamentos_dotnet_dio.Models;
 
+
+// Conversao para tipo string
+//int inteiro = 5;
+//string a = inteiro.ToString(); // melhor pratica para converter para string.
+// nao tem parse para string, e o convert e redundante.
+
+// Cast implicito
+// int a = 5;
+// double b = a;
+// inteiro cabe no double, por isso ele faz o casting implicito. (sem ver)
+// long a = 5;
+// int b = a;
+// nao permite o casting implicito, sera necessario realizar a conversao.
+// long a = 5;
+// int b = Convert.ToInt32(a);
+
+
+// Cast - Casting
+// Converter uma string para inteiro.
+// 1 forma
+//int a = Convert.ToInt32("5");
+// 2 forma
+// int a = int.Parse("5");
+
+// Diferencia entre convert e parse
+// parse nao aceita valor null.
+// convert ele aceita null e retorna 0.
+
+
+// Operador de atribuição(= sinal de atribuição, quando quer modificar o valor da variável.)
+// int a = 10;
+// int b = 20;
+
+// int c = a + b;
+// c = c + 5; // 30 + 5
+// c += 5; // representara a mesma coisa, esta acrescentando mais 5, somando os valores.
+// c -= 5; // subtrai, decremento.
+// Console.WriteLine(c);
+//**********************************************************//
+
 // // pega a data atual da maquina em uso.
 // DateTime dataAtual = DateTime.Now;
 // Console.WriteLine(dataAtual);
-
+//*********************************************************//
 
 
 // string aprensentacao = "Seja Bem Vindo";
@@ -23,7 +63,7 @@ using fundamentos_dotnet_dio.Models;
 // Console.WriteLine("Valor da variavel altura: " + altura);
 // Console.WriteLine("Valor da variavel preco: " + preco);
 // Console.WriteLine("Valor da variavel condicao: " + condicao);
-
+//************************************************************************//
 
 
 // instanciando a classe Pessoa
